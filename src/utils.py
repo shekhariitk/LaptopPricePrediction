57db598eee4a170b35e06e39fb4d51aad39d6fda
shekhar suman <you@example.com>
2024-12-19 18:43:33 +0530

    utills

--- src/utils.py
+++ src/utils.py
@@ -27,7 +27,7 @@ def evaluate_model(X_train,y_train, X_test, y_test, models):
         report = {}
         for i in range(len(list(models))):
              model = list(models.values())[i]
-             model.fit(X_test, y_test)
+             model.fit(X_train, y_train)
 
             # Make Prediction
 
