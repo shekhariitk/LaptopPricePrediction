43daf4b5e63e45781f647477ce94048883a35d4f
shekhar suman <you@example.com>
2024-12-31 18:36:00 +0530

    mlflow added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,3 +1,6 @@
+import mlflow
+import mlflow.sklearn
+
 import pandas as pd
 import numpy as np
 
@@ -19,136 +22,129 @@ from src.utils import evaluate_model
 import os, sys
 from dataclasses import dataclass
 
-## Model Trainning Configuration
-
-@dataclass
-class ModelTrainerconfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
-
-## Model Training Class
 class ModelTrainerClass:
     def __init__(self):
-        self.model_trainer_config  = ModelTrainerconfig()
+        self.model_trainer_config = ModelTrainerconfig()
 
-    def  initiate_model_training(self,train_arr, test_arr):
+    def initiate_model_training(self, train_arr, test_arr):
         try:
-            logging.info("Splitting independent and Dependent Variable")
-            X_train,y_train, X_test, y_test = (
-                train_arr[:,:-1],
-                np.log(train_arr[:,-1]),
-                test_arr[:,:-1],
-                np.log(test_arr[:,-1])
-
+            logging.info("Splitting independent and dependent variables")
+            X_train, y_train, X_test, y_test = (
+                train_arr[:, :-1],
+                np.log(train_arr[:, -1]),
+                test_arr[:, :-1],
+                np.log(test_arr[:, -1]),
             )
 
-            models  = {
-
-                "LinearRegression":LinearRegression(),
-                "Lasso":Lasso(),
+            models = {
+                "LinearRegression": LinearRegression(),
+                "Lasso": Lasso(),
                 "Ridge": Ridge(),
-                "DecisionTreeRegressor":DecisionTreeRegressor(),
+                "DecisionTreeRegressor": DecisionTreeRegressor(),
                 "ElasticNet": ElasticNet(),
-                "RandomForestRegressor":RandomForestRegressor(),
-                "KNeighborsRegressor":KNeighborsRegressor(),
-                "DecisionTreeRegressor":DecisionTreeRegressor(),
-                "GradientBoostingRegressor":GradientBoostingRegressor(),
-                "AdaBoostRegressor":AdaBoostRegressor(),
-                "ExtraTreesRegressor":ExtraTreesRegressor(),
-                "SVR":SVR(),
-                "XGBRegressor":XGBRegressor(n_estimators=45,max_depth=5,learning_rate=0.5)
-    
-    }
-            params={
+                "RandomForestRegressor": RandomForestRegressor(),
+                "KNeighborsRegressor": KNeighborsRegressor(),
+                "GradientBoostingRegressor": GradientBoostingRegressor(),
+                "AdaBoostRegressor": AdaBoostRegressor(),
+                "ExtraTreesRegressor": ExtraTreesRegressor(),
+                "SVR": SVR(),
+                "XGBRegressor": XGBRegressor(n_estimators=45, max_depth=5, learning_rate=0.5),
+            }
+
+            params = {
                 "DecisionTreeRegressor": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    'splitter':['best','random'],
-                    'max_features':['sqrt','log2'],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_features': ['sqrt', 'log2'],
                 },
-                "GradientBoostingRegressor":{
-                    #'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    #'criterion':['squared_error', 'friedman_mse'],
-                    #'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,45,64,128,256]
+                "GradientBoostingRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 45, 64, 128, 256],
                 },
-                "LinearRegression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "LinearRegression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
                 },
-                "AdaBoostRegressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "AdaBoostRegressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'loss': ['linear', 'square', 'exponential'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
                 },
-                "KNeighborsRegressor":{
+                "KNeighborsRegressor": {
                     'n_neighbors': [3, 5, 10, 20],
                     'weights': ['uniform', 'distance'],
-                    'metric': ['minkowski', 'euclidean', 'manhattan']
+                    'metric': ['minkowski', 'euclidean', 'manhattan'],
                 },
-                "ExtraTreesRegressor":{
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                "ExtraTreesRegressor": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features': ['sqrt', 'log2', None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
                 },
-                "Lasso":{
-                    'alpha': [np.random.uniform(0.01, 10)], 
+                "Lasso": {
+                    'alpha': [np.random.uniform(0.01, 10)],
                     'max_iter': [1000, 2000, 5000],
-                    'fit_intercept': [True, False]},
-
-                "Ridge":{
+                    'fit_intercept': [True, False],
+                },
+                "Ridge": {
                     'alpha': [0.1, 1, 10, 100],
-                    'solver': ['auto', 'svd', 'lsqr']
+                    'solver': ['auto', 'svd', 'lsqr'],
                 },
-                "SVR":{
-                    'kernel':['rbf'],
-                    'C':[1,10,100,10000],
-                    'epsilon':[0.1,0.2]
+                "SVR": {
+                    'kernel': ['rbf'],
+                    'C': [1, 10, 100, 10000],
+                    'epsilon': [0.1, 0.2],
                 },
-                "ElasticNet":{},
-                "DecisionTreeRegressor":{
+                "ElasticNet": {},
+                "DecisionTreeRegressor": {
                     'max_depth': [5, 10, 15, None],
                     'min_samples_split': [2, 5, 10],
                     'min_samples_leaf': [1, 2, 4],
-                    'max_features': ['auto', 'sqrt', 'log2']},
-
-                "RandomForestRegressor":{
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "RandomForestRegressor": {
                     'n_estimators': [np.random.randint(100, 1000)],
                     'max_depth': [None, 10, 20, 30],
                     'min_samples_split': [np.random.randint(2, 20)],
                     'min_samples_leaf': [np.random.randint(1, 20)],
                     'max_features': ['auto', 'sqrt', 'log2'],
-                    'bootstrap': [True, False]
-                }
-                        
-                        }
-            model_report : dict = evaluate_model(X_train,y_train, X_test, y_test, models,param=params)
-            print(model_report)
-            print("\n ==================================================================================")
-            logging.info(f"Model report info : {model_report}")
+                    'bootstrap': [True, False],
+                },
+            }
 
-            ## To get best model from model dictionary
-            best_model_score = max(sorted(model_report.values()))
+            # Start MLflow experiment
+            mlflow.set_experiment("Model_Training")
+            mlflow.set_tracking_uri("http://127.0.0.1:5000")
+            with mlflow.start_run() as run :
+                logging.info("Evaluating models and hyperparameter tuning")
+                model_report: dict = evaluate_model(X_train, y_train, X_test, y_test, models, param=params)
 
-            best_model_name  = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            best_model = models[best_model_name]
+                print(model_report)
+                logging.info(f"Model report: {model_report}")
 
-            print(f"Best model found , Best model name is {best_model_name} and that R2 Score: {best_model_score}")
-            print("\n=================================================================")
-            logging.info(f"Best model found , Best model name is {best_model_name} and that R2 Score: {best_model_score}")
+                # Get the best model and its score
+                best_model_score = max(sorted(model_report.values()))
+                best_model_name = list(model_report.keys())[
+                    list(model_report.values()).index(best_model_score)
+                ]
+                best_model = models[best_model_name]
 
+                print(f"Best model: {best_model_name} with R2 Score: {best_model_score}")
+                logging.info(f"Best model: {best_model_name} with R2 Score: {best_model_score}")
 
-            save_obj(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
+                # Log the best model's parameters and metrics
+                mlflow.log_param("best_model_name", best_model_name)
+                mlflow.log_metric("best_model_score", best_model_score)
 
-        except   Exception as e:
-            logging.info("Error occured in model trainer path") 
+                # Save and log the best model
+                save_obj(
+                    file_path=self.model_trainer_config.trained_model_file_path,
+                    obj=best_model,
+                )
+                mlflow.sklearn.log_model(best_model, "best_model")
+
+        except Exception as e:
+            logging.error("Error occurred during model training")
             raise CustomException(e, sys)
-        
-        
+
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import pickle
 import pandas as pd
 import numpy as np
+import mlflow
+import mlflow.sklearn
 
 from src.exception import CustomException
 from src.logger import logging
@@ -23,38 +25,68 @@ def save_obj(file_path, obj):
         raise CustomException(e, sys)   
 
 
-def evaluate_model(X_train,y_train, X_test, y_test, models,param):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
+    """
+    Evaluates multiple models using GridSearchCV for hyperparameter tuning.
+    Logs the results and model artifacts to MLflow.
+
+    Args:
+        X_train (array): Training feature data.
+        y_train (array): Training target data.
+        X_test (array): Testing feature data.
+        y_test (array): Testing target data.
+        models (dict): Dictionary of model names and corresponding model objects.
+        param (dict): Dictionary of model names and their respective hyperparameter grids.
+
+    Returns:
+        dict: A dictionary containing model names as keys and their R2 scores as values.
+    """
     try:
         report = {}
+        
         for i in range(len(list(models))):
-             model = list(models.values())[i]
-             para=param[list(models.keys())[i]]
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param.get(model_name, {})
 
-             logging.info(f"model:{model} is started")
+            logging.info(f"Evaluating model: {model_name}")
 
-             gs = GridSearchCV(model,para,cv=3)
-             gs.fit(X_train,y_train)
+            # Start an MLflow run for the current model
+            with mlflow.start_run(run_name=model_name):
+                # Perform GridSearchCV for hyperparameter tuning
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, scoring='r2', verbose=1)
+                gs.fit(X_train, y_train)
 
-             model.set_params(**gs.best_params_)
-             model.fit(X_train,y_train)
+                # Set the best parameters to the model
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
-             logging.info(f"model:{model} is Evaluated and best param is {gs.best_params_}")
+                logging.info(f"Model: {model_name} best parameters: {gs.best_params_}")
 
-            # Make Prediction
+                # Log best parameters to MLflow
+                mlflow.log_params(gs.best_params_)
 
-             y_pred = model.predict(X_test)
+                # Make predictions on the test set
+                y_pred = model.predict(X_test)
+                logging.info(f"Model: {model_name} predictions completed")
 
-             logging.info(f"model:{model} prediction is completed")
+                # Calculate R2 score
+                test_model_score = r2_score(y_test, y_pred)
+                report[model_name] = test_model_score
 
-             test_model_score = r2_score(y_test,y_pred)
-             report[list(models.keys())[i]] = test_model_score
+                logging.info(f"Model: {model_name} R2 score: {test_model_score}")
 
-             logging.info(f"model:{model} score is stored and the socre is : {test_model_score}")
+                # Log the R2 score to MLflow
+                mlflow.log_metric("R2_score", test_model_score)
+                mlflow.log_metric("MSE", mean_squared_error(y_test,y_pred))               
+
+                # Log the trained model to MLflow
+                mlflow.sklearn.log_model(model, model_name)
 
         return report  
 
     except Exception as e: 
-        logging.info("Error Occured during model Training ")
+        logging.error("Error occurred during model evaluation")
         raise CustomException(e, sys)
     
 
